a3d1074d063a6031c9c4364d441b33837acfe760
seungju <youseungjua@gmail.com>
2022-02-19 01:11:27 +0900

    22.02.18

--- C/10250.c
+++ C/10250.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    
+    int T,H,W,N;
+    scanf("%d",&T);
+    while(T--){
+        scanf("%d %d %d",&H,&W,&N);
+        int p = N/H; //
+        int q = N%H; //0
+        if(q==0) printf("%d\n",H*100+p);
+        else printf("%d\n",q*100+p+1);
+    }
+    return 0;
 }
