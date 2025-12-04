4ef4b8cdc3f48df6ebe9b1a696e074e65a5ffa29
mokjakA <youseungjua@gmail.com>
2025-12-04 20:11:50 +0900

    2025-12-04-thr feat(11659): 구간합구하기4 / Solved

--- JavaScript/[S3]11659/index.js
+++ JavaScript/[S3]11659/index.js
@@ -1,22 +1,18 @@
 const fs = require("fs");
-const input = fs
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number);
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const [N, M] = input[0].split(" ");
-const sumMap = new Array.from(new Array(N));
-const arr = input[1].split(" ");
+const [N, M] = input[0].split(" ").map(Number);
+const arr = input[1].split(" ").map(Number);
+const prefixSum = new Array(N + 1).fill(0);
 
-sumMap;
-
-for (let i = 0; i < M; i++) {
-  let sum = 0;
-  const [s, e] = input[2 + i].split(" ");
-  for (let j = s - 1; j < e; j++) {
-    sum += arr[j];
-  }
-  console.log(sum);
+for (let i = 0; i < N; i++) {
+  prefixSum[i + 1] = prefixSum[i] + arr[i];
 }
+
+// I/O 메모리 절약
+const result = [];
+for (let t = 0; t < M; t++) {
+  const [s, e] = input[2 + t].split(" ").map(Number);
+  result.push(prefixSum[e] - prefixSum[s - 1]);
+}
+console.log(result.join("\n"));
