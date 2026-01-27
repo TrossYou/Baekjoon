4feabb9a1639382f48679125ca2cc85908a46ada
youseungjua <youseungjua@gmail.com>
2026-01-27 13:04:08 +0900

    2026-01-27-tue feat(11049):행렬곱셈순서

--- Java/[G3]11049/Main.java
+++ Java/[G3]11049/Main.java
@@ -1,25 +1,29 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] arr= new int[N][2];
+        int[] arr= new int[N+1];
         for(int i = 0 ; i < N ; i++){
-            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(i == N-1) arr[N] = Integer.parseInt(st.nextToken());
         }
 
         int[][] dp = new int[N][N];
-        for(int i = 0 ; i < N ; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
-        for(int len = 1; len < N ; len++){
-            for(int i = 0 ; i < N ; i++){
-                int j = i + len;
-                if(j < N){
-                    
+
+        for(int len = 1; len < N ; len++){ 
+            for(int i = 0 ; i + len < N ; i++){ 
+                int j = i + len; 
+                dp[i][j] = Integer.MAX_VALUE;
+                for(int k = i ; k < j ; k++){ 
+                    int cost = dp[i][k] + dp[k+1][j] + arr[i] * arr[k+1] * arr[j+1];
+                    dp[i][j] = Math.min(dp[i][j], cost);
                 }
             }
         }
-
+        System.out.println(dp[0][N-1]);
     }
 }
