e7b0c05f9a865c137093f7e7f7183da895f50a9c
mokjakA <youseungjua@gmail.com>
2026-01-30 00:09:22 +0900

    2026-01-29-thr feat(12100):2048(easy)

---
+++ Java/[B5]25083/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder();
+    System.out.print(sb.append("         ,r'\"7").append("\n")
+        .append("r`-_   ,'  ,/ ").append("\n")
+        .append(" \\. \". L_r'").append("\n")
+        .append("   `~\\/").append("\n")
+        .append("      |").append("\n")
+        .append("      |").append("\n"));
+  }
+}
