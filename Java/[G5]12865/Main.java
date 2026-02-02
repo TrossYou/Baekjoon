88967ffd4ba44616154bdb270b06be11d6698fcd
youseungjua <youseungjua@gmail.com>
2026-02-02 17:51:58 +0900

    2026-02-02-mon feat(12865):평범한배낭 / WIP

--- Java/[G5]12865/Main.java
+++ Java/[G5]12865/Main.java
@@ -4,26 +4,34 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>(); // 배열이 훨씬 효율적
+        int[] values;
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        values = new int[K+1]; //무게가 idx일때 가치 저장
         int[] dp = new int[K+1]; // 남은 용량이 idx일 때, 최대의 가치 저장
 
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
-            int key = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
             int value = Integer.parseInt(st.nextToken());
-            map.put(key, value);
-            
+            values[weight] = value;
         }
 
-
         for(int i = 1 ; i < K+1 ; i++){
-            // dp[i] = map.getOrDefault(i,0);// 현재 무게의 물건이 있다면 저장 -> getOrDefault 메서드 알게 됨
-            dp[i] = Math.max(dp[i-1] + dp[1], map.getOrDefault(i, 0)); // dp최대값 계산
+            // dp[i] = map.getOrDefault(i, 0);
+            // for(int j = 1 ; j <= i/2 ; j++){
+            //     dp[i] = Math.max(dp[i-j] + dp[j], dp[i]); // dp최대값 계산
+            // }
+            for(int  k = K ; k > 0 ; k--){
+                if(values[k] != 0) {
+                    int v = values[k];
+                    dp[i] = Math.max(dp[i], dp[i-k] + v);
+                }
+            }
         }
 
         System.out.println(dp[K]);
