b840ba093ce97a027a20c70c2b7ff69f770f02b5
mokjakA <youseungjua@gmail.com>
2026-03-31 21:10:57 +0900

    2026-03-31-tue swea(32645):동까뚱뽭게임

--- Java/G3_32645/Main.java
+++ Java/G3_32645/Main.java
@@ -31,7 +31,9 @@ public class Main {
     boolean[] isVisited = new boolean[N + 1];
     int[] order = new int[N]; // bfs순서 -> 추후 깊이 탐색에서도 사용
     int[] parent = new int[N + 1];
+    int[] depth = new int[N + 1]; // i노드의 깊이
     boolean[] isLeaf = new boolean[N + 1];
+
     Arrays.fill(isLeaf, true);
 
     int head = 0, tail = 0;
@@ -39,11 +41,13 @@ public class Main {
     order[tail++] = ROOT; // 루트노드
     isVisited[ROOT] = true;
     parent[ROOT] = ROOT;
+    depth[ROOT] = 0;
 
     while (head < tail) {
       int curr = order[head++];
       for (int node : graph.get(curr)) {
         if (!isVisited[node]) { // 방문하지 않은 노드라면, order에 추가+Parent
+          depth[node] = depth[curr] + 1;
           isVisited[node] = true;
           order[tail++] = node;
           parent[node] = curr;
@@ -52,24 +56,26 @@ public class Main {
       }
     }
 
-    int[] minDepth = new int[N + 1];
-    Arrays.fill(minDepth, MAX_VALUE);
+    boolean[] dp = new boolean[N + 1]; // 동우 이김:true
 
     for (int i = tail - 1; i >= 0; i--) {
       int curr = order[i];
       if (isLeaf[curr]) {
-        minDepth[curr] = 0;
+        continue;
       }
 
-      int p = parent[curr];
-      if (p != curr) { // 루트 노드라면,,?? 루트는..본인을 업데이트 하면 안됨
-        minDepth[p] = Math.min(minDepth[p], minDepth[curr] + 1);
+      // 자식을 돌며, dp가 true인게 있으면 true 아니면 false
+      for (int next : graph.get(curr)) {
+        if (next != parent[curr] && !dp[next]) {
+          dp[curr] = true;
+          break;
+        }
       }
     }
 
     // 출력
     for (int i = 1; i <= N; i++) {
-      sb.append(minDepth[i] % 2 == 0 ? "uppercut" : "donggggas").append('\n');
+      sb.append(dp[i] ? "donggggas" : "uppercut").append('\n');
     }
 
     System.out.print(sb);
