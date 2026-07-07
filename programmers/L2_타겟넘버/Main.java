47a5ee8180844f99f8358bd8a57f9dc1ef3096e8
mokjakA <youseungjua@gmail.com>
2026-07-07 16:40:07 +0900

    폴더 구조 조정

---
+++ programmers/L2_타겟넘버/Main.java
@@ -0,0 +1,11 @@
+package L2_타겟넘버;
+
+public class Main {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] numbers = { 1, 1, 1, 1, 1 };
+
+    System.out.println(solution.solution(numbers, 3));
+    return;
+  }
+}
