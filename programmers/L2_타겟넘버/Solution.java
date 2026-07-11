e66d7785e348d268031fd1f12edabc7dec28d01d
mokjakA <youseungjua@gmail.com>
2026-07-11 23:28:27 +0900

    260711SAT 프로그래머스 L2:타겟넘버

--- programmers/L2_타겟넘버/Solution.java
+++ programmers/L2_타겟넘버/Solution.java
@@ -1,49 +1,29 @@
 package L2_타겟넘버;
 
 class Solution {
-  boolean[] ops;
-  int[] nums;
-  int len, targetV, ans;
+  int finalA = 0;
+  int finalT;
 
   public int solution(int[] numbers, int target) {
-    len = numbers.length;
-    ops = new boolean[len]; // true: +, false: -
-    nums = numbers;
-    targetV = target;
+    finalT = target;
+    int maxMask = (int) Math.pow(2, numbers.length);
 
-    for (int mask = 0; mask < (1 << len); mask++) {
-      calc(mask); // 비트마스크 활용
-    }
+    for (int mask = 0; mask < maxMask; mask++)
+      calc(numbers, mask);
 
-    return ans;
+    return finalA;
   }
 
-  // public void dfs(int depth) {
-  // if (depth == len) {
-  // return;
-  // }
-
-  // ops[depth] = true;
-  // calc();
-
-  // dfs(depth + 1);
-
-  // ops[depth] = false;
-  // calc();
-  // }
-
-  public void calc(int mask) { // 계산 중
-    int res = nums[0];
-    for (int i = 0; i < len; i++) {
-      if ((mask & (1 << i)) != 0) {
-        res += nums[i];
-      } else {
-        res -= nums[i];
-      }
+  public void calc(int[] numbers, int mask) {
+    int sum = 0;
+    for (int i = 0; i < numbers.length; i++) {
+      if ((mask & (1 << i)) != 0)
+        sum += numbers[i];
+      else
+        sum -= numbers[i];
     }
-
-    if (res == targetV)
-      ans++;
+    if (sum == finalT)
+      finalA++;
+    return;
   }
-
 }
